23556092e888ae62933919047c218c609544b303
Sujianxing <1731625223@qq.com>
2022-04-26 09:36:57 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.atguigu.git;
+
+/**
+ * @author jiangxingsu
+ * @create 2022-04-26-9:28
+ */
+public class GitTest {
+
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
